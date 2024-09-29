178f9a64ce43b0ebce98734005976a9a1fba14d8
Fandi Ardyan <96646749+FFF9ep@users.noreply.github.com>
2024-09-29 22:31:46 +0700

    Praktikum Pemrograman Lanjut S3

---
+++ Proglan/Modul 1/Tugas/Tugas 4/tugas4/src/main/java/MaxFinder.java
@@ -0,0 +1,18 @@
+public class MaxFinder {
+    public static int findMax(int a, int b, int c) {
+        if (a >= b && a >= c) {
+            return a;
+        } else if (b >= a && b >= c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Test to Find Max Positive: " + (findMax(1, 2, 3) == 3));
+        System.out.println("Test to Find Max Negative: " + (findMax(-1, -2, -3) == -1));
+        System.out.println("Test to Find Max With Zero: " + (findMax(0, 0, 1) == 1));
+
+    }
+}
